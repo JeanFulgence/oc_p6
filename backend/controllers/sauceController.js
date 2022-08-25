48c7c20e946cf2952c01311509576b1678645437
JeanFulgence <josselin.veret@gmail.com>
2022-08-25 16:38:35 +0200

    Optimisation de la fonction like/dislike

--- backend/controllers/sauceController.js
+++ backend/controllers/sauceController.js
@@ -3,7 +3,7 @@ const Sauce = require("../models/sauceModel");
 const fs = require("fs");
 
 // Création d'une expression régulière pour la saisie des champs de la sauce
-let regex = /^[a-zA-Z0-9 _.,!&]+$/;
+let regex = /^[A-Za-zÀ-ÖØ-öø-ÿ'0-9 _.,!?&]+$/;
 
 // Fonction de récupération de toutes les sauces
 exports.getAllSauces = (req, res, next) => {
@@ -50,12 +50,25 @@ exports.createSauce = (req, res, next) => {
 
 // Fonction de modification d'une sauce
 exports.modifySauce = (req, res, next) => {
-     const sauceObject = req.file ? {
-         ...JSON.parse(req.body.sauce),
-         imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`} : { ...req.body };
-     Sauce.updateOne({ _id : req.params.id}, {...sauceObject, _id: req.params.id})
-     .then(res.status(200).json({ message : "Sauce modifiée"}))
-     .catch(error => res.status(400).json({ error }))
+     
+     // Récupération de la sauce via l'id de la requête
+     Sauce.findOne({ _id: req.params.id })
+     .then(sauce => {
+          // Vérification de la correspondance entre créateur de la sauce et l'utilisateur qui souhaite la modifier
+          if (sauce.userId != req.auth.userId) {
+               res.status(401).json({message: "Non autorisé."});
+
+          // Mise à jour de la sauce
+          } else {
+               const sauceObject = req.file ? {
+               ...JSON.parse(req.body.sauce),
+               imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`} : { ...req.body };
+               Sauce.updateOne({ _id : req.params.id}, {...sauceObject, _id: req.params.id})
+               .then(res.status(200).json({ message : "Sauce modifiée"}))
+               .catch(error => res.status(400).json({ error }));
+          }
+     })
+     .catch( error => res.status(500).json({ error }));
 }
 
 // Fonction de suppression d'une sauce
@@ -89,56 +102,99 @@ exports.opinionOnSauce = (req, res, next) => {
 
           // Ajout d'un like et de l'id de l'utilisateur dans les propriétés "likes" et "usersLiked" de la sauce si on reçoit "1" du front
           case 1 :
+               Sauce.findOne({ _id: req.params.id })
+               .then((sauce) => {
 
-               // Push de l'id de l'utilisateur dans "usersLiked" et incrémentation de "likes"
-               Sauce.updateOne(
-                    { _id: req.params.id },
-                    { $push: { usersLiked: req.body.userId },
-                    $inc: { likes: +1 }})
-               .then(() => res.status(200).json({ message: "Like!" }))
-               .catch((error) => res.status(400).json({ error }))
-                    
+                    // Vérifie si l'utilisateur a déjà mis un like à la sauce
+                    if (sauce.usersLiked.includes(req.body.userId)) {
+                         res.status(401).json({message: "Non autorisé."});
+
+                    /* Si l'utilisateur a mis un "dislike", retire son id de "usersDisliked", ajoute -1 aux "dislikes" puis ajout de son id dans "usersLiked" et incrémentation de "likes".
+                    Le code du frontend prévenant ce cas de figure, cette partie permet de s'assurer qu'aucun dysfonctionnement n'intervienne en cas de modification du frontend. */
+                    } else if (sauce.usersDisliked.includes(req.body.userId)) {
+                         Sauce.updateOne(
+                              { _id: req.params.id },
+                              { $push: { usersLiked: req.body.userId }, 
+                              $pull: { usersDisliked: req.body.userId },
+                              $inc: { likes: +1, dislikes: -1 }})
+                         .then(() => res.status(200).json({ message: "Like!" }))
+                         .catch((error) => res.status(400).json({ error }));
+
+                    // Ajout de l'id de l'utilisateur dans "usersLiked" et incrémentation de "likes"
+                    } else {
+                         Sauce.updateOne(
+                              { _id: req.params.id },
+                              { $push: { usersLiked: req.body.userId },
+                              $inc: { likes: +1 }})
+                         .then(() => res.status(200).json({ message: "Like!" }))
+                         .catch((error) => res.status(400).json({ error }))
+                    }
+               })
+          .catch((error) => res.status(404).json({ error }));
           break;
         
           case 0 :
                Sauce.findOne({ _id: req.params.id })
                .then((sauce) => {
 
-               // Si l'utilisateur enlève son like
-               if (sauce.usersLiked.includes(req.body.userId)) { 
-                    Sauce.updateOne(
-                         { _id: req.params.id },
-                         { $pull: { usersLiked: req.body.userId },
-                         $inc: { likes: -1 }})
-                    .then(() => res.status(200).json({ message: "None." }))
-                    .catch((error) => res.status(400).json({ error }))
-               }
-               
-               // Si l'utilisateur enlève son dislike
-               if (sauce.usersDisliked.includes(req.body.userId)) { 
-                    Sauce.updateOne(
-                         { _id: req.params.id },
-                         { $pull: { usersDisliked: req.body.userId },
-                         $inc: { dislikes: -1 }})
-                    .then(() => res.status(200).json({ message: "None." }))
-                    .catch((error) => res.status(400).json({ error }))
+                    // Si l'utilisateur enlève son like
+                    if (sauce.usersLiked.includes(req.body.userId)) { 
+                         Sauce.updateOne(
+                              { _id: req.params.id },
+                              // Retrait de l'id de l'utilisateur de usersLiked
+                              { $pull: { usersLiked: req.body.userId },
+                              $inc: { likes: -1 }})
+                         .then(() => res.status(200).json({ message: "None." }))
+                         .catch((error) => res.status(400).json({ error }));
+                    }
+                    
+                    // Si l'utilisateur enlève son dislike
+                    if (sauce.usersDisliked.includes(req.body.userId)) { 
+                         Sauce.updateOne(
+                              { _id: req.params.id },
+                              // Retrait de l'id de l'utilisateur de usersDisliked
+                              { $pull: { usersDisliked: req.body.userId },
+                              $inc: { dislikes: -1 }})
+                         .then(() => res.status(200).json({ message: "None." }))
+                         .catch((error) => res.status(400).json({ error }));
                     }
                })
-                  .catch((error) => res.status(404).json({ error }))
+               .catch((error) => res.status(404).json({ error }));
           break;
         
           // Ajout d'un dislike et de l'id de l'utilisateur dans les propriétés "dislikes" et "usersDisliked" de la sauce si on reçoit "-1" du front
           case -1 :
-               
-               // Push de l'id de l'utilisateur dans "usersDisliked" et incrémentation de "dislikes"
-               Sauce.updateOne(
-                    { _id: req.params.id },
-                    { $push: { usersDisliked: req.body.userId },
-                    $inc: { dislikes: +1 }})
-               .then(() => { res.status(200).json({ message: "Yikes!" }) })
-               .catch((error) => res.status(400).json({ error }))
+          Sauce.findOne({ _id: req.params.id })
+               .then((sauce) => {
+
+                    // Vérifie si l'utilisateur a déjà mis un dislike à la sauce
+                    if (sauce.usersDisliked.includes(req.body.userId)) {
+                         res.status(401).json({message: "Non autorisé."});
+                    
+                    /* Si l'utilisateur a mis un "like", retire son id de "usersLiked", ajoute -1 aux "likes" puis ajout de son id dans "usersDisliked" et incrémentation de "dislikes".
+                    Le code du frontend prévenant ce cas de figure, cette partie permet de s'assurer qu'aucun dysfonctionnement n'intervienne en cas de modification du frontend. */
+                    } else if (sauce.usersLiked.includes(req.body.userId)) {
+                         Sauce.updateOne(
+                              { _id: req.params.id },
+                              { $push: { usersDisliked: req.body.userId }, 
+                              $pull: { usersLiked: req.body.userId },
+                              $inc: { dislikes: +1, likes: -1 }})
+                         .then(() => res.status(200).json({ message: "Yikes!" }))
+                         .catch((error) => res.status(400).json({ error }));
+
+                    // Ajout de l'id de l'utilisateur dans "usersLiked" et incrémentation de "likes"
+                    } else {
+                         Sauce.updateOne(
+                              { _id: req.params.id },
+                              { $push: { usersDisliked: req.body.userId },
+                              $inc: { dislikes: +1 }})
+                         .then(() => { res.status(200).json({ message: "Yikes!" }) })
+                         .catch((error) => res.status(400).json({ error }));
+                    }
+               })
+               .catch((error) => res.status(404).json({ error }));
           break;
-              
+
           default:
           console.log(error);
      }
